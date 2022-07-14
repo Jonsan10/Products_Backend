fd2ec0189a891ef856e18321a3e9de7b7c6497ac
jonathan sanchez <sanchezjon480@gmail.com>
2022-07-14 12:09:05 -0700

    registered Car Model with Admin site + Created Post functionality for product

--- products/views.py
+++ products/views.py
@@ -1,8 +1,21 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ProductSerializers
+from .models import Product
 
-@api_view(['GET'])
+
+@api_view(['GET', 'POST'])
 def products_list(request):
+    
+    if request.method == 'Get':
+        products = Product.objects.all()
+        serializer = ProductSerializers(products, many=True)
+        return Response(serializer.data)
 
 
-    return Response('ok')
+    elif request.method == 'POST':
+        serializer = ProductSerializers(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status= status.HTTP_201_CREATED)
